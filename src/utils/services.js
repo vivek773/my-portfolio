dcfb7e9cef1bb9e3df67cc1c7afb276eb516ec38
nirali-chavda <chavdanirali18@gmail.com>
2024-05-12 01:32:31 +0530

    API services add

---
+++ src/utils/services.js
@@ -0,0 +1,36 @@
+
+import axios from "axios";
+import { BASE_API_URL } from "./Constants";
+import store from "../store/index";
+
+const getStoreData = () => {
+  const { accessToken } = store.getState().auth;
+  return { accessToken };
+};
+
+const handleRequest = async (method, URL, body) => {
+  const { accessToken } = getStoreData();
+  try {
+    const headers = {};
+    if (accessToken) {
+      headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    headers['Content-Type'] = 'application/json';
+    const response = await method(BASE_API_URL + URL, body, { headers });
+    return response.data
+  } catch (error) {}
+};
+
+export const fetchPOSTRequest = async (URL, body = {}) => {
+  return handleRequest(axios.post, URL, body);
+};
+
+export const fetchPUTRequest = async (URL, body = {}) => {
+  return handleRequest(axios.put, URL, body);
+};
+
+export const fetchDELETERequest = async (URL, body = {}) => {
+  return handleRequest(axios.delete, URL, body);
+};
+
+
