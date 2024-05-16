29f7e07c459acd61fcc85ffca1f5942ec3fa8320
kartik_chavda <kc72284@gmail.com>
2024-05-16 19:03:17 +0530

    detail page UI added

--- src/routes.js
+++ src/routes.js
@@ -10,16 +10,32 @@ import Page404 from "./pages/404/404Page";
 import DashboardLayout from "./layout/layout";
 import FleetPage from "./pages/fleet/FleetPage";
 import AddPlaneComponent from "./components/fleet/AddPlaneComponent";
+import DetailComponent from "./components/detail/DetailComponent";
+
+// Protected Routes
+import ProtectedRoute from "./protectedroute/ProtectedRoute";
 
 const MainRouter = () => {
+  
+  const wrapWithProtectedRoute = (children) => {
+    return <ProtectedRoute>{children}</ProtectedRoute>;
+  };
+
   return (
     <Routes>
       <Route path={"register"} element={<SignupPage />} />
       <Route path={"login"} element={<LoginPage />} />
       <Route path={"*"} element={<Page404 />} />
-      <Route path={"/"} element={<DashboardLayout />}>
-        <Route path={"fleet"} element={<FleetPage />} />
-        <Route path={"fleet/add-plane"} element={<AddPlaneComponent />} />
+      <Route path={"/"} element={wrapWithProtectedRoute(<DashboardLayout />)}>
+        <Route path={"fleet"} element={wrapWithProtectedRoute(<FleetPage />)} />
+        <Route
+          path={"fleet/add-plane"}
+          element={wrapWithProtectedRoute(<AddPlaneComponent />)}
+        />
+        <Route
+          path="/fleet/:id"
+          element={wrapWithProtectedRoute(<DetailComponent />)}
+        />
       </Route>
     </Routes>
   );
