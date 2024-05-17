10d6a6b06bbae927df44525e0635993fa86cd125
Sheheryar Ahmad <sheriahmad@hotmail.com>
2024-05-17 00:05:14 -0700

    added prettier

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
-// Constants 
+// Constants
 
-// API url
+// API urls
 export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 export const WEB_PORTAL_URL = process.env.REACT_APP_WEB_PORTAL_URL;
 
