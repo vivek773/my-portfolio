770436176eb2e308ebe07588c4d5011d075060a6
kartik_chavda <kc72284@gmail.com>
2024-05-27 14:52:31 +0530

    table changes

--- src/pages/destinations/DestinationsPage.js
+++ src/pages/destinations/DestinationsPage.js
@@ -1,56 +1,53 @@
-import {
-  Button,
-  Card,
-  CircularProgress,
-  Container,
-  Stack,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  Typography,
-  Chip,
-  Box,
-} from "@mui/material";
+// Destination page
+
+// Default
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+
+// MUI components
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
 import { setDestinations } from "../../store/features/DestinationsSlice";
-import { fetchGETRequest } from "../../utils/Services";
-import { useLoader } from "../../context/LoaderContext";
-import { CHIP } from "../../utils/Color"; // Assuming your theme file is in this path
+
+// Custom
 import Label from "../../components/label";
 import CustomButton from "../../forms/button/CustomButton";
+import SpinnerComponent from "../../components/spinner/SpinnerComponent";
+
+// Utils
+import { fetchGETRequest } from "../../utils/Services";
+import { renderChipColorByStatus } from "../../utils/Helper";
+ 
+// Context
+import { useLoader } from "../../context/LoaderContext";
+
 
 const TABLE_HEAD = [
-  { id: "number", label: "#", alignRight: false },
-  { id: "city", label: "City", alignRight: false },
-  { id: "state", label: "State", alignRight: false },
-  { id: "country", label: "Country", alignRight: false },
-  { id: "airport_name", label: "Airport Name", alignRight: false },
-  { id: "airport_code", label: "Airport Code", alignRight: false },
-  { id: "airport_latitude", label: "Airport Latitude", alignRight: false },
-  { id: "airport_longitude", label: "Airport Longitude", alignRight: false },
-  { id: "status", label: "Status", alignRight: false },
-  { id: "details", label: "Details", alignRight: true },
+  { id: "number", label: "#" },
+  { id: "city", label: "City" },
+  { id: "state", label: "State" },
+  { id: "country", label: "Country" },
+  { id: "airport_name", label: "Airport Name" },
+  { id: "airport_code", label: "Airport Code" },
+  { id: "airport_latitude", label: "Airport Latitude" },
+  { id: "airport_longitude", label: "Airport Longitude" },
+  { id: "status", label: "Status" },
+  { id: "details", label: "Details" },
 ];
 
-const renderChipColorByStatus = (status) => {
-  if (status === "active") {
-    return "success";
-  } else if (status === "inactive") {
-    return "error";
-  } else if (status === "invited") {
-    return "info";
-  } else if (status === "banned") {
-    return "error";
-  } else if (status === "inactive") {
-    return "warning";
-  }
-};
 
 export default function DestinationsPage() {
   const [page, setPage] = useState(0);
@@ -98,7 +95,7 @@ export default function DestinationsPage() {
               transform: "translate(-50%, -50%)",
             }}
           >
-            <CircularProgress />
+            <SpinnerComponent show={isLoading} />
           </div>
         )}
 
@@ -123,73 +120,87 @@ export default function DestinationsPage() {
           />
         </Stack>
 
-        <Card>
-          <TableContainer sx={{ minWidth: 800 }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {TABLE_HEAD.map((header) => (
-                    <TableCell key={header.id} align="center">
-                      {header.label}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {Array.isArray(destinations) &&
-                  destinations?.map((destination, index) => (
-                    <TableRow hover key={destination.destination_id}>
-                      <TableCell align="center">{index + 1}</TableCell>
-                      <TableCell align="center">{destination.city}</TableCell>
-                      <TableCell align="center">{destination.state}</TableCell>
-                      <TableCell align="center">
-                        {destination.country}
+        {destinations?.length === 0 ? (
+          <Box>
+            <Typography
+              variant="h6"
+              gutterBottom
+              mt={10}
+              fontWeight={500}
+              textAlign={"center"}
+              color={"Gray"}
+            >
+              No data available
+            </Typography>
+          </Box>
+        ) : (
+          <Card>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    {TABLE_HEAD.map((header) => (
+                      <TableCell key={header.id} align="center">
+                        {header.label}
                       </TableCell>
-                      <TableCell align="center">
-                        {destination.airport_name}
-                      </TableCell>
-                      <TableCell align="center">
-                        {destination.airport_code}
-                      </TableCell>
-                      <TableCell align="center">
-                        {destination.airport_latitude}
-                      </TableCell>
-                      <TableCell align="center">
-                        {destination.airport_longitude}
-                      </TableCell>
-                      <TableCell align="center">
-                        <Label
-                          color={
-                            // (status === "banned" && "inactive") || "success"
-                            renderChipColorByStatus(destination.status)
-                          }
-                        >
-                          {destination.status}
-                        </Label>
-                      </TableCell>
-                      <TableCell align="center">
-                        <CustomButton
-                          width={"fit-content"}
-                          onClick={() => handleView(destination)}
-                          label={"View"}
-                          size={"small"}
-                        />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {Array.isArray(destinations) &&
+                    destinations?.map((destination, index) => (
+                      <TableRow hover key={destination.destination_id}>
+                        <TableCell align="center">{index + 1}</TableCell>
+                        <TableCell align="center">{destination.city}</TableCell>
+                        <TableCell align="center">
+                          {destination.state}
+                        </TableCell>
+                        <TableCell align="center">
+                          {destination.country}
+                        </TableCell>
+                        <TableCell align="center">
+                          {destination.airport_name}
+                        </TableCell>
+                        <TableCell align="center">
+                          {destination.airport_code}
+                        </TableCell>
+                        <TableCell align="center">
+                          {destination.airport_latitude}
+                        </TableCell>
+                        <TableCell align="center">
+                          {destination.airport_longitude}
+                        </TableCell>
+                        <TableCell align="center">
+                          <Label
+                            color={renderChipColorByStatus(destination.status)}
+                          >
+                            {destination.status}
+                          </Label>
+                        </TableCell>
+                        <TableCell align="center">
+                          <CustomButton
+                            width={"fit-content"}
+                            onClick={() => handleView(destination)}
+                            label={"View"}
+                            size={"small"}
+                          />
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
 
-          <TablePagination
-            rowsPerPageOptions={[10]}
-            component="div"
-            count={destinations.length}
-            rowsPerPage={limit}
-            page={page}
-            onPageChange={(event, newPage) => setPage(newPage)}
-          />
-        </Card>
+            <TablePagination
+              rowsPerPageOptions={[10]}
+              component="div"
+              count={destinations.length}
+              rowsPerPage={limit}
+              page={page}
+              onPageChange={(event, newPage) => setPage(newPage)}
+            />
+          </Card>
+        )}
       </Container>
     </>
   );
