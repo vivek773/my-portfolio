46919b158d1993b6a3de0fd2adb1fed3ee6ed77e
Sheheryar Ahmad <sheriahmad@hotmail.com>
2024-05-27 20:50:41 -0700

    Dynamci schedule dates

--- src/pages/calendar/CalendarPage.js
+++ src/pages/calendar/CalendarPage.js
@@ -12,7 +12,6 @@ import {
   Resize,
   DragAndDrop,
 } from "@syncfusion/ej2-react-schedule";
-import { extend } from "@syncfusion/ej2-base";
 import { Typography, CircularProgress } from "@mui/material";
 import HelmetComponent from "../../components/helmet/HelmetComponent";
 import { EDISPATCHED } from "../../utils/Constants";
@@ -24,65 +23,91 @@ const CalendarPage = () => {
   const [data, setData] = useState([]);
   const [PilotData, setPilotData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const getBookingsData = async (startDate, endDate) => {
+    setIsLoading(true);
+    try {
+      const response = await fetchGETRequest(
+        `/schedule/owner/get-schedule?start_date=${startDate.toISOString()}&end_date=${endDate.toISOString()}`
+      );
+
+      if (
+        response?.statusCode === 200 &&
+        response.schedule &&
+        response.fleet &&
+        response.pilots
+      ) {
+        const bookings = response.schedule;
+        const planes = response.fleet;
+        const pilots = response.pilots;
+
+        const scheduleData = bookings.map((item) => ({
+          Id: item.schedule_id,
+          Subject:
+            `${item.booking.customer.first_name} ${item.booking.customer.last_name} - ${item.booking.booking_reference} ` ||
+            "No Title",
+          StartTime: new Date(item.booking_start_time),
+          EndTime: new Date(item.booking_end_time),
+          TailNumber: "plane",
+          PilotId: item.tail_number,
+        }));
+
+        const planeData = planes.map((plane) => ({
+          text: plane.tail_number,
+          id: plane.tail_number,
+          groupId: "plane",
+          color: COLOR_OBJECT.primary,
+        }));
+
+        const pilotData = pilots.map((pilot, index) => ({
+          text: `${pilot.user.first_name} ${pilot.user.last_name}`,
+          id: pilot.user_id,
+          groupId: "pilot",
+          color: index % 2 === 0 ? "#ea7a57" : "#5978ee",
+        }));
+
+        setData(scheduleData);
+        setPilotData([...planeData, ...pilotData]);
+      } else {
+        console.error("Error fetching data:", response);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const getBookingsData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetchGETRequest(
-          `/schedule/owner/get-schedule?start_date=2024-05-08&end_date=2024-07-15`,
-          {}
-        );
+    const startDate = new Date(selectedDate);
+    startDate.setDate(startDate.getDate() - 1);
+    const endDate = new Date(startDate);
+    endDate.setMonth(endDate.getMonth() + 1);
+    endDate.setDate(endDate.getDate() + 1);
 
-        if (
-          response?.statusCode === 200 &&
-          response.schedule &&
-          response.fleet &&
-          response.pilots
-        ) {
-          const bookings = response.schedule;
-          const planes = response.fleet;
-          const pilots = response.pilots;
-
-          const scheduleData = bookings.map((item) => ({
-            Id: item.schedule_id,
-            Subject:
-              `${item.booking.customer.first_name} ${item.booking.customer.last_name} - ${item.booking.booking_reference} ` ||
-              "No Title",
-            StartTime: new Date(item.booking_start_time),
-            EndTime: new Date(item.booking_end_time),
-            TailNumber: "plane",
-            PilotId: item.tail_number,
-          }));
-
-          const planeData = planes.map((plane) => ({
-            text: plane.tail_number,
-            id: plane.tail_number,
-            groupId: "plane",
-            color: COLOR_OBJECT.primary,
-          }));
-
-          const pilotData = pilots.map((pilot, index) => ({
-            text: `${pilot.user.first_name} ${pilot.user.last_name}`,
-            id: pilot.user_id,
-            groupId: "pilot",
-            color: index % 2 === 0 ? "#ea7a57" : "#5978ee",
-          }));
-
-          setData(scheduleData);
-          setPilotData([...planeData, ...pilotData]);
-        } else {
-          console.error("Error fetching data:", response);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getBookingsData();
+    getBookingsData(startDate, endDate);
   }, []);
+
+  const handleDateChange = (args) => {
+    setSelectedDate(args.value);
+  };
+
+  const handleActionComplete = (args) => {
+    if (
+      args.requestType === "viewNavigate" ||
+      args.requestType === "dateNavigate"
+    ) {
+      const newDate = args.currentDate;
+      const startDate = new Date(newDate);
+      startDate.setDate(startDate.getDate() - 1);
+      const endDate = new Date(startDate);
+      endDate.setMonth(endDate.getMonth() + 1);
+      endDate.setDate(endDate.getDate() + 1);
+
+      getBookingsData(startDate, endDate);
+    }
+  };
 
   const workDays = [0, 1, 2, 3, 4, 5];
 
@@ -118,11 +143,17 @@ const CalendarPage = () => {
                 height="auto"
                 rowAutoHeight={true}
                 showQuickInfo={false}
-                selectedDate={new Date(2024, 4, 31)}
+                selectedDate={selectedDate}
                 currentView="TimelineWeek"
                 workDays={workDays}
-                eventSettings={{ dataSource: data, ignoreWhitespace: true }}
+                eventSettings={{
+                  dataSource: data,
+                  ignoreWhitespace: true,
+                  allowEditing: false,
+                }}
                 group={{ resources: ["Planes", "Pilots"] }}
+                dateChange={handleDateChange}
+                actionComplete={handleActionComplete}
               >
                 <ResourcesDirective>
                   <ResourceDirective
